7528961c83c2f82203c4f565ff3b8d4873af173c
Simone Cusimano <s.cusimano@avanade.com>
2016-01-25 19:00:58 +0100

    Initial commit

---
+++ app/src/test/java/com/unica/so2/enotesrecorder/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.unica.so2.enotesrecorder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
