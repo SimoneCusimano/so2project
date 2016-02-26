274c34e6e0347af8e4adeb2ce19771e03edbe72e
s.cusimano <simone.cusimano92@gmail.com>
2016-02-26 22:21:25 +0100

    Added DbHelper and DbAdabter.

---
+++ app/src/main/java/com/unica/so2/enotesrecorder/DAL/DbAdapter.java
@@ -0,0 +1,139 @@
+package com.unica.so2.enotesrecorder.DAL;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.unica.so2.enotesrecorder.Helper.DatabaseHelper;
+
+public class DbAdapter {
+
+    private static final String DATABASE_TABLE = "note";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_DATE = "last_edit";
+    public static final String KEY_BODY = "body";
+    public static final String KEY_ID = "_id";
+    public static final String KEY_AUDIO = "audio";
+    public static final String KEY_RATING = "rating";
+
+    private DatabaseHelper mDbHelper;
+    private SQLiteDatabase mDb;
+
+    private final Context mCtx;
+
+    /**
+     * Constructor - takes the context to allow the database to be
+     * opened/created
+     *
+     * @param ctx the Context within which to work
+     */
+    public DbAdapter(Context ctx) {
+        this.mCtx = ctx;
+    }
+
+    /**
+     * Open the notes database. If it cannot be opened, try to create a new
+     * instance of the database. If it cannot be created, throw an exception to
+     * signal the failure
+     *
+     * @return this (self reference, allowing this to be chained in an
+     *         initialization call)
+     * @throws SQLException if the database could be neither opened or created
+     */
+    public DbAdapter open() throws SQLException {
+        mDbHelper = new DatabaseHelper(mCtx);
+        mDb = mDbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        mDbHelper.close();
+    }
+
+
+    /**
+     * Create a new note using the title and body provided. If the note is
+     * successfully created return the new rowId for that note, otherwise return
+     * a -1 to indicate failure.
+     *
+     * @param title the title of the note
+     * @param body the body of the note
+     * @return rowId or -1 if failed
+     */
+    public long createNote(String title, String body, String date, String audio, String rating) {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_TITLE, title);
+        initialValues.put(KEY_BODY, body);
+        initialValues.put(KEY_DATE, date);
+        initialValues.put(KEY_AUDIO, audio);
+        initialValues.put(KEY_RATING, rating);
+        return mDb.insert(DATABASE_TABLE, null, initialValues);
+    }
+
+    /**
+     * Delete the note with the given rowId
+     *
+     * @param rowId id of note to delete
+     * @return true if deleted, false otherwise
+     */
+    public boolean deleteNote(long rowId) {
+
+        return mDb.delete(DATABASE_TABLE, KEY_ID + "=" + rowId, null) > 0;
+    }
+
+    /**
+     * Return a Cursor over the list of all notes in the database
+     *
+     * @return Cursor over all notes
+     */
+    public Cursor fetchAllNotes() {
+
+        return mDb.query(DATABASE_TABLE, new String[] {KEY_ID, KEY_TITLE,
+                KEY_BODY,KEY_DATE}, null, null, null, null, null);
+    }
+
+    /**
+     * Return a Cursor positioned at the note that matches the given rowId
+     *
+     * @param rowId id of note to retrieve
+     * @return Cursor positioned to matching note, if found
+     * @throws SQLException if note could not be found/retrieved
+     */
+    public Cursor fetchNote(long rowId) throws SQLException {
+
+        Cursor mCursor =
+
+                mDb.query(true, DATABASE_TABLE, new String[] {KEY_ID,
+                                KEY_TITLE, KEY_BODY,KEY_DATE}, KEY_ID + "=" + rowId, null,
+                        null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+
+    }
+
+    /**
+     * Update the note using the details provided. The note to be updated is
+     * specified using the rowId, and it is altered to use the title and body
+     * values passed in
+     *
+     * @param rowId id of note to update
+     * @param title value to set note title to
+     * @param body value to set note body to
+     * @return true if the note was successfully updated, false otherwise
+     */
+    public boolean updateNote(long rowId, String title, String body,String date) {
+        ContentValues args = new ContentValues();
+        args.put(KEY_TITLE, title);
+        args.put(KEY_BODY, body);
+
+        //This lines is added for personal reason
+        args.put(KEY_DATE, date);
+
+        //One more parameter is added for data
+        return mDb.update(DATABASE_TABLE, args, KEY_ID + "=" + rowId, null) > 0;
+    }
+}
