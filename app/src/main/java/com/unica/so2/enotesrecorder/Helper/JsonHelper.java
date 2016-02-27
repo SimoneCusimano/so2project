4ec7346391a49f1db45ff3d26479beeff27e822a
s.cusimano <simone.cusimano92@gmail.com>
2016-02-27 19:51:58 +0100

    Fixed JsonHelper

--- app/src/main/java/com/unica/so2/enotesrecorder/Helper/JsonHelper.java
+++ app/src/main/java/com/unica/so2/enotesrecorder/Helper/JsonHelper.java
@@ -1,25 +1,46 @@
 package com.unica.so2.enotesrecorder.Helper;
 
 
+import com.unica.so2.enotesrecorder.Model.Content;
+
+import org.json.JSONObject;
+
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 
 public class JsonHelper {
 
         /*serialization: from Content to Json string*/
-        public String serializeContent (Content content) {
+        public String serializeContent(Content content) {
+
             String jsonString = "";
+
             try {
-                FileOutputStream fout = new FileOutputStream("thenote.note");
-                ObjectOutputStream oos = new ObjectOutputStream(fout);
-                oos.writeObject(note);
-                oos.close();
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("description", content.getDescription());
+                jsonObject.put("audio", content.getAudio());
+                jsonString = jsonObject.toString();
             }
-            catch (Exception e) { e.printStackTrace(); }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
             return jsonString;
         }
+
         // deserialization: from Json string to Content
         public Content deserializeContent (String jsonString) {
+            Content content = new Content();
 
+            try {
+                JSONObject json = new JSONObject(jsonString);
+                content.setDescription(json.getString("description"));
+                content.setAudio(json.getString("audio"));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return content;
         }
 }
